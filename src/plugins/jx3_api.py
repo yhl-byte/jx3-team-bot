fcba7d2622de324e12b2ca1808cc307d1faec633
yhl <yuhailong@thalys-tech.onaliyun.com>
2025-05-09 10:40:08 +0800

    feat(角色属性): 增加角色图标和背景颜色显示
    在角色属性页面中，新增了角色图标的显示，并根据角色门派动态设置背景颜色。同时调整了侧边栏的宽度和字体大小，优化了页面布局。

--- src/plugins/jx3_api.py
+++ src/plugins/jx3_api.py
@@ -10,9 +10,10 @@ import aiohttp
 import json
 from typing import Dict, List, Optional
 from jx3api import JX3API,AsyncJX3API
-from ..utils.index import format_daily_data,format_role_data,path_to_base64
-from .html_generator import render_role_attribute
+from ..utils.index import format_daily_data,format_role_data,path_to_base64,render_team_template,get_code_by_name
+from .html_generator import render_role_attribute,img_to_base64
 from .render_image import generate_html_screenshot
+from src.config import STATIC_PATH
 # from html2image import Html2Image
 import os
 
@@ -67,7 +68,24 @@ async def handle_role_detail(bot: Bot, event: GroupMessageEvent, state: T_State)
     # 发送处理提示
     processing_msg = await bot.send(event=event, message="正在生成属性信息，请稍候...")
     res = await async_api.role_attribute(server= server_name, name=role_name)
+    colors = render_team_template().get("colors_by_mount_name")
+    kungfu_name = res.get('kungfuName', '')
+    if '·悟' in kungfu_name:
+        xf_name = kungfu_name.replace('·悟', '')
+    else:
+        xf_name = kungfu_name
+
+    if '·' in kungfu_name:
+        icon_name = kungfu_name.replace('·', '')
+    else:
+        icon_name = kungfu_name
+    xf_icon = img_to_base64(STATIC_PATH.absolute() / f"xf-cn-icon/{icon_name.strip()}.png")
+    card = await async_api.show_cache(server= server_name, name=role_name)
+    print(11,card)
     roleInfo = {
+        "color": colors.get(xf_name, "#e8e8e8"),
+        "xfIcon": xf_icon,
+        "show": card.get('showAvatar', ''),
         "serverName": res.get('serverName', '未知服务器'),
         "roleName": res.get('roleName', '未知角色'),
         "kungfuNam": res.get('kungfuName', '未知等级'),
@@ -84,11 +102,10 @@ async def handle_role_detail(bot: Bot, event: GroupMessageEvent, state: T_State)
             if panel.get("name") not in ["会心", "会心效果", "破防", "无双", "破招", "加速"]
         ]
     }
-    print(res.get('equipList', []))
-     # 生成 HTML 内容
+    # 生成 HTML 内容
     html_content = render_role_attribute(roleInfo)
     # # 转换为图片
-    image_path = await generate_html_screenshot(html_content, 1000)
+    image_path = await generate_html_screenshot(html_content, 1200)
     # # 发送图片
     await RoleAttribute.finish(MessageSegment.image(path_to_base64(image_path)))
     # 清理临时文件
